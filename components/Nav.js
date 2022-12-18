6fb1e22ff8bfa71403e38e76cc2a187fe9b882f6
Jay Snyder <jaysnyder23@gmail.com>
2022-12-18 09:55:30 -0600

    january updates

--- components/Nav.js
+++ components/Nav.js
@@ -21,7 +21,6 @@ export default function Nav(props) {
             <nav className='w-full max-w-[1400px] flex flex-row justify-between items-center mx-auto px-10 py-5 relative'>
                 <div className='flex flex-row justify-start items-end'>
                   <Link href="/" passHref={true}><a className='font-bold text-pink-500 mr-10'>baked by<span className='font-serif font-normal text-black text-3xl leading-0 ml-2'>Mary</span></a></Link>
-                  <Link href="/about-us" passHref={true}><a className='hidden md:flex h-full hover:underline hover:decoration-pink-500 hover:underline-offset-4 transition-all mr-5'>about us</a></Link>
                   <Menu as="div" className="text-left whitespace-nowrap">
                     <div className='text-amber-800 transition-all ease-in-out group'>
                       <Menu.Button className="hidden md:flex text-black flex-row justify-start items-center h-full hover:underline hover:decoration-pink-500 hover:underline-offset-4 transition-all mr-5">
@@ -118,6 +117,7 @@ export default function Nav(props) {
                       </Menu.Items>
                     </Transition>
                   </Menu>
+                  <Link href="/about-us" passHref={true}><a className='hidden md:flex h-full hover:underline hover:decoration-pink-500 hover:underline-offset-4 transition-all mr-5'>about us</a></Link>
                 </div>
                 <div className='flex flex-row justify-end items-center space-x-4'>
                   <SubscribeForm />
