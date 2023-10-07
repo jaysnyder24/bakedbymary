6ba7fc754e0a3e3ec352d58031261acea79cbd81
Jay Snyder <jaysnyder23@gmail.com>
2023-10-06 22:56:35 -0500

    desktop attempt 7

--- app/nav.jsx
+++ app/nav.jsx
@@ -1,7 +1,7 @@
 import { ShoppingBagIcon, FacebookIcon, InstagramIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import CartNotice from "./cartNotice";
+import CartNotice from "./CartNotice";
 
 export default function Nav () {
 
