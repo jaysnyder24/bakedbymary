eca3de472742a825772e287b8dfd6d80c5811578
Jay Snyder <jaysnyder23@gmail.com>
2023-10-06 23:14:25 -0500

    added ga4

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { Poppins, Playfair_Display } from 'next/font/google'
 import Nav from "./nav";
 import Footer from "./footer";
 import CartProvider from "../contexts/CartContext";
+import Script from "next/script";
 
 const poppins = Poppins({
     subsets: ['latin'],
@@ -21,6 +22,16 @@ const poppins = Poppins({
 export default function RootLayout ({children}) {
     return ( 
         <html lang="en" className={`${poppins.variable} ${playfair.variable}`}>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-QDQ49V35MB" />
+            <Script id="google-analytics">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+        
+                gtag('config', 'G-QDQ49V35MB');
+                `}
+            </Script>
             <body className="overflow-x-hidden">
                 <CartProvider>
                     <Nav />
