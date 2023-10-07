e88ba329ee71f2a195ea8635565ecde246d9da96
Jay Snyder <jaysnyder23@gmail.com>
2023-10-06 23:02:41 -0500

    quick nav fix

--- app/nav.jsx
+++ app/nav.jsx
@@ -10,7 +10,7 @@ export default function Nav () {
             <div className="flex flex-row justify-between items-center ring-1 ring-pink-300 bg-white shadow-2xl shadow-pink-100 py-4 px-5 w-full rounded-full relative">
                 <div className="flex flex-row justify-start items-center space-x-5">
                     <Link href={"/cookies"} className="px-2 py-1 no-underline hover:underline decoration-pink-400 underline-offset-8 decoration-4">cookies</Link>
-                    <Link href={"#"} className="px-2 py-1 no-underline hover:underline decoration-pink-400 underline-offset-8 decoration-4">About Us</Link>
+                    <Link href={"#"} className="px-2 py-1 no-underline hover:underline decoration-pink-400 underline-offset-8 decoration-4">about us</Link>
                 </div>
                 <Link href={"/"} className="bg-pink-600 hover:bg-pink-700 w-14 h-14 rounded-full flex flex-row justify-center items-center transition-colors duration-300 absolute top-auto bottom-auto mx-auto inset-x-0"><Image src="/images/logo.svg" className="fill-white stroke-white text-white" width={32} height={32} alt="baked by mary cookies logo" /></Link>
                 <div className="flex flex-row justify-end items-center space-x-5">
