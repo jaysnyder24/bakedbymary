5397a37b9fc62d2084fb482320c4e78c63036106
Jay Snyder <jaysnyder23@gmail.com>
2024-03-28 20:39:28 -0500

    nav updates

--- app/ui/navigation/InteractiveNavMenu.jsx
+++ app/ui/navigation/InteractiveNavMenu.jsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { ArrowRightIcon } from "lucide-react";
-import { useState } from "react";
-import Image from "next/image";
+import Link from 'next/link';
+import { ArrowRightIcon } from 'lucide-react';
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function InteractiveNavMenu(props) {
   const [focus, setFocus] = useState(0);
@@ -13,10 +13,10 @@ export default function InteractiveNavMenu(props) {
   return (
     <div
       onMouseLeave={() => setFocus(0)}
-      className="absolute overflow-hidden group-hover/nav-parent:overflow-visible flex h-0 group-hover/nav-parent:h-auto top-full pt-2 z-[100]"
+      className='absolute overflow-hidden group-hover/nav-parent:overflow-visible flex h-0 group-hover/nav-parent:h-auto top-full pt-2 z-[100]'
     >
-      <div className="bg-white rounded-lg p-5 ring-0 group-hover/nav-parent:ring-1 ring-stone-100 flex justify-start items-start space-x-5 shadow-none group-hover/nav-parent:shadow-lg opacity-0 -translate-y-4 group-hover/nav-parent:opacity-100 group-hover/nav-parent:translate-y-0 delay-200 transition-all duration-300 ease-in-out">
-        <div className="flex flex-col justify-start items-start space-y-5">
+      <div className='bg-white rounded-lg p-5 ring-0 group-hover/nav-parent:ring-1 ring-stone-100 flex justify-start items-start space-x-5 shadow-none group-hover/nav-parent:shadow-lg opacity-0 -translate-y-4 group-hover/nav-parent:opacity-100 group-hover/nav-parent:translate-y-0 delay-200 transition-all duration-300 ease-in-out'>
+        <div className='flex flex-col justify-start items-start space-y-5'>
           <Link
             href={`/cookies`}
             onMouseOver={() => {
@@ -25,18 +25,18 @@ export default function InteractiveNavMenu(props) {
             className={`bg-repeat ${
               focus == 0
                 ? 'bg-[url("/images/tileDark.png")] text-white'
-                : "bg-white text-stone-500"
+                : 'bg-white text-stone-500'
             } hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies`}
           >
-            <span className="font-poppins font-bold text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
+            <span className='font-poppins font-bold text-sm group-hover/lineup-cookies:text-white flex justify-start items-center'>
               Lineup Cookies
               <ArrowRightIcon
                 className={`h-3 w-3 ${
-                  focus == 0 ? "ml-3" : "ml-2"
+                  focus == 0 ? 'ml-3' : 'ml-2'
                 } transition-all duration-300 ease-in-out`}
               />
             </span>
-            <span className="font-poppins text-xs group-hover/lineup-cookies:text-white">
+            <span className='font-poppins text-xs group-hover/lineup-cookies:text-white'>
               This month’s lineup of curated cookies.
             </span>
           </Link>
@@ -48,50 +48,27 @@ export default function InteractiveNavMenu(props) {
             className={`bg-repeat ${
               focus == 1
                 ? 'bg-[url("/images/tileDark.png")] text-white'
-                : "bg-white text-stone-500"
+                : 'bg-white text-stone-500'
             } hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies`}
           >
-            <span className="font-poppins font-bold text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
+            <span className='font-poppins font-bold text-sm group-hover/lineup-cookies:text-white flex justify-start items-center'>
               All Cookies
               <ArrowRightIcon
                 className={`h-3 w-3 ${
-                  focus == 1 ? "ml-3" : "ml-2"
+                  focus == 1 ? 'ml-3' : 'ml-2'
                 } transition-all duration-300 ease-in-out`}
               />
             </span>
-            <span className="font-poppins text-xs group-hover/lineup-cookies:text-white">
+            <span className='font-poppins text-xs group-hover/lineup-cookies:text-white'>
               Luxurious list of all our delicious cookies.
-            </span>
-          </Link>
-          <Link
-            href={`/cookies`}
-            onMouseOver={() => {
-              setFocus(2);
-            }}
-            className={`bg-repeat ${
-              focus == 2
-                ? 'bg-[url("/images/tileDark.png")] text-white'
-                : "bg-white text-stone-500"
-            } hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies`}
-          >
-            <span className="font-poppins font-bold text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
-              Cookie Categories
-              <ArrowRightIcon
-                className={`h-3 w-3 ${
-                  focus == 2 ? "ml-3" : "ml-2"
-                } transition-all duration-300 ease-in-out`}
-              />
-            </span>
-            <span className="font-poppins text-xs group-hover/lineup-cookies:text-white">
-              No what you like but not what you want? Check here!
             </span>
           </Link>
         </div>
         <div
           className={`${
             focus == 0
-              ? "grid opacity-100 translate-x-0 w-auto"
-              : "hidden opacity-0 translate-x-2 w-0"
+              ? 'grid opacity-100 translate-x-0 w-auto'
+              : 'hidden opacity-0 translate-x-2 w-0'
           } grid-flow-row grid-cols-6 grid-rows-4 min-w-[600px] h-full flex-wrap gap-5`}
         >
           {lineupProducts.map((product, index) => {
@@ -99,11 +76,11 @@ export default function InteractiveNavMenu(props) {
               <Link
                 href={`/cookies/${product.metadata.slug}`}
                 key={index}
-                className="col-span-3 row-span-2 flex justify-start items-center space-x-4 group"
+                className='col-span-3 row-span-2 flex justify-start items-center space-x-4 group'
               >
-                <div className="h-full w-5/12 flex flex-col justify-center items-center relative">
+                <div className='h-full w-5/12 flex flex-col justify-center items-center relative'>
                   <div className="w-2/3 h-2/3 group-hover:h-full rounded-lg bg-[url('/images/tileLight.png')] bg-cover absolute top-0 left-0 overflow-hidden transition-all duration-300 ease-in-out"></div>
-                  <div className="w-10/12 aspect-square rounded-lg z-50 group-hover:translate-x-2 relative transition-transform duration-300 ease-in-out">
+                  <div className='w-3/4 aspect-square rounded-lg z-50 group-hover:translate-x-2 relative transition-transform duration-300 ease-in-out'>
                     <Image
                       src={`/images/${product.metadata.imageUnique}Circle.png`}
                       fill
@@ -111,17 +88,17 @@ export default function InteractiveNavMenu(props) {
                     ></Image>
                   </div>
                 </div>
-                <div className="h-full w-1/4 flex-grow flex flex-col justify-center items-start space-y-1">
-                  <span className="font-playfair font-black text-xl text-pink-950">
+                <div className='h-full w-1/4 flex-grow flex flex-col justify-center items-start space-y-1'>
+                  <span className='font-playfair font-black text-xl text-pink-950'>
                     {product.name}
                   </span>
-                  <span className="font-poppins font-semibold text-pink-700">
+                  <span className='font-poppins font-semibold text-pink-700'>
                     ${product.default_price.unit_amount / 100}
                     {(product.default_price.unit_amount / 100)
                       .toString()
-                      .split("").length > 2
-                      ? "0"
-                      : ".00"}
+                      .split('').length > 2
+                      ? '0'
+                      : '.00'}
                     / half dozen
                   </span>
                 </div>
@@ -131,7 +108,7 @@ export default function InteractiveNavMenu(props) {
         </div>
         <div
           className={`${
-            focus == 1 ? "grid" : "hidden"
+            focus == 1 ? 'grid' : 'hidden'
           } grid-flow-row grid-cols-6 grid-rows-6 min-w-[600px] h-full flex-wrap gap-5`}
         >
           {specialProducts.map((product, index) => {
@@ -139,11 +116,11 @@ export default function InteractiveNavMenu(props) {
               <Link
                 href={`/cookies/${product.metadata.slug}`}
                 key={index}
-                className="col-span-2 row-span-2 flex justify-start items-center space-x-3 group"
+                className='col-span-2 row-span-2 flex justify-start items-center space-x-3 group'
               >
-                <div className="h-5/6 aspect-square flex flex-col justify-center items-center relative">
+                <div className='h-5/6 aspect-square flex flex-col justify-center items-center relative'>
                   <div className="w-2/3 h-2/3 group-hover:h-full rounded-lg bg-[url('/images/tileLight.png')] bg-cover absolute top-0 left-0 overflow-hidden transition-all duration-300 ease-in-out"></div>
-                  <div className="w-10/12 aspect-square rounded-lg z-50 group-hover:translate-x-2 relative transition-transform duration-300 ease-in-out">
+                  <div className='w-10/12 aspect-square rounded-lg z-50 group-hover:translate-x-2 relative transition-transform duration-300 ease-in-out'>
                     <Image
                       src={`/images/${product.metadata.imageUnique}Circle.png`}
                       fill
@@ -151,24 +128,14 @@ export default function InteractiveNavMenu(props) {
                     ></Image>
                   </div>
                 </div>
-                <div className="h-full w-1/4 flex-grow flex flex-col justify-center items-start space-y-1">
-                  <span className="font-playfair font-black text-pink-950">
+                <div className='h-full w-1/4 flex-grow flex flex-col justify-center items-start space-y-1'>
+                  <span className='font-playfair font-black text-pink-950'>
                     {product.name}
                   </span>
                 </div>
               </Link>
             );
           })}
-        </div>
-        <div
-          className={`${
-            focus == 2 ? "grid" : "hidden"
-          } grid-flow-row grid-cols-6 grid-rows-4 min-w-[600px] h-full flex-wrap gap-5`}
-        >
-          <Link href={`#`} className="bg-pink-500 col-span-3 row-span-2"></Link>
-          <Link href={`#`} className="bg-pink-500 col-span-3 row-span-2"></Link>
-          <Link href={`#`} className="bg-pink-500 col-span-3 row-span-2"></Link>
-          <Link href={`#`} className="bg-pink-500 col-span-3 row-span-2"></Link>
         </div>
       </div>
     </div>
--- app/ui/navigation/Nav.tsx
+++ app/ui/navigation/Nav.tsx
@@ -2,17 +2,17 @@ import {
   ChevronDownIcon,
   ArrowRightIcon,
   ShoppingCartIcon,
-} from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import CartNotice from "../../CartNotice";
-import InteractiveNavMenu from "./InteractiveNavMenu";
+} from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import CartNotice from '../../CartNotice';
+import InteractiveNavMenu from './InteractiveNavMenu';
 
 async function getProducts() {
-  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
   const products = await stripe.products.list({
     active: true,
-    expand: ["data.default_price"],
+    expand: ['data.default_price'],
     limit: 100,
   });
 
@@ -23,74 +23,74 @@ export default async function Nav() {
   const products = await getProducts();
 
   const lineupProducts = products.filter((item) => {
-    return item.metadata.available === "lineup";
+    return item.metadata.available === 'lineup';
   });
 
   const specialProducts = products.filter((item) => {
-    return item.metadata.available === "special";
+    return item.metadata.available === 'special';
   });
 
   specialProducts.length = 9;
 
   const limitedProducts = products.filter((item) => {
-    return item.metadata.available === "limited";
+    return item.metadata.available === 'limited';
   });
 
   const saleProducts = products.filter((item) => {
-    return item.metadata.available !== "other";
+    return item.metadata.available !== 'other';
   });
 
   return (
-    <nav className="w-full px-10 md:px-16 flex justify-center items-center relative">
-      <section className="w-full max-w-7xl flex justify-between items-center py-7 z-10">
-        <ul className="flex justify-start items-center">
-          <li className="mr-14">
+    <nav className='w-full px-10 md:px-16 flex justify-center items-center relative'>
+      <section className='w-full max-w-7xl flex justify-between items-center py-7 z-10'>
+        <ul className='flex justify-start items-center'>
+          <li className='mr-14'>
             <Link
               href={`/`}
-              className="rounded-full bg-pink-500 hover:bg-pink-700 h-12 w-12 flex justify-center items-center transition-colors duration-300 ease-in-out"
+              className='rounded-full bg-pink-500 hover:bg-pink-700 h-12 w-12 flex justify-center items-center transition-colors duration-300 ease-in-out'
             >
               <Image
                 src={`/images/logo.svg`}
-                className="fill-white stroke-white text-white"
+                className='fill-white stroke-white text-white'
                 width={26}
-                alt="baked by mary cookies logo"
+                alt='baked by mary cookies logo'
                 height={20}
               ></Image>
             </Link>
           </li>
-          <li className="mr-7 relative group/nav-parent">
+          <li className='mr-7 relative group/nav-parent'>
             <Link
               href={`/cookies`}
-              className="font-poppins font-semibold text-black flex justify-start items-center px-2 py-2"
+              className='font-poppins font-semibold text-black flex justify-start items-center px-2 py-2'
             >
               cookies
-              <ChevronDownIcon className="text-pink-500 h-4 w-4 ml-1 group-hover/nav-parent:-rotate-180 transition-all duration-300 ease-in-out" />
+              <ChevronDownIcon className='text-pink-500 h-4 w-4 ml-1 group-hover/nav-parent:-rotate-180 transition-all duration-300 ease-in-out' />
             </Link>
             <InteractiveNavMenu
               lineupProducts={lineupProducts}
               specialProducts={specialProducts}
             />
           </li>
-          <li className="mr-7 relative group/nav-parent">
+          <li className='mr-7 relative group/nav-parent'>
             <Link
               href={`/catering`}
-              className="font-poppins font-semibold text-black flex justify-start items-center px-2 py-2"
+              className='font-poppins font-semibold text-black flex justify-start items-center px-2 py-2'
             >
               catering
-              <ChevronDownIcon className="text-pink-500 h-4 w-4 ml-1 group-hover/nav-parent:-rotate-180 transition-all duration-300 ease-in-out" />
+              <ChevronDownIcon className='text-pink-500 h-4 w-4 ml-1 group-hover/nav-parent:-rotate-180 transition-all duration-300 ease-in-out' />
             </Link>
-            <div className="absolute overflow-hidden group-hover/nav-parent:overflow-visible flex h-0 group-hover/nav-parent:h-auto top-full pt-2 z-[100]">
-              <div className="bg-white rounded-lg p-5 ring-0 group-hover/nav-parent:ring-1 ring-stone-100 flex justify-start items-start space-x-5 shadow-none group-hover/nav-parent:shadow-lg opacity-0 -translate-y-4 group-hover/nav-parent:opacity-100 group-hover/nav-parent:translate-y-0 delay-200 transition-all duration-300 ease-in-out">
-                <div className="flex flex-col justify-start items-start space-y-3">
+            <div className='absolute overflow-hidden group-hover/nav-parent:overflow-visible flex h-0 group-hover/nav-parent:h-auto top-full pt-2 z-[100]'>
+              <div className='bg-white rounded-lg p-5 ring-0 group-hover/nav-parent:ring-1 ring-stone-100 flex justify-start items-start space-x-5 shadow-none group-hover/nav-parent:shadow-lg opacity-0 -translate-y-4 group-hover/nav-parent:opacity-100 group-hover/nav-parent:translate-y-0 delay-200 transition-all duration-300 ease-in-out'>
+                <div className='flex flex-col justify-start items-start space-y-3'>
                   <Link
                     href={`#`}
                     className="bg-repeat bg-white hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies transition-all duration-300 ease-in-out"
                   >
-                    <span className="font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
+                    <span className='font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center'>
                       Special Orders
-                      <ArrowRightIcon className="h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out" />
+                      <ArrowRightIcon className='h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out' />
                     </span>
-                    <span className="font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white">
+                    <span className='font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white'>
                       Not interested in this month’s cookies? No problem!
                     </span>
                   </Link>
@@ -98,11 +98,11 @@ export default async function Nav() {
                     href={`#`}
                     className="bg-repeat bg-white hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies transition-all duration-300 ease-in-out"
                   >
-                    <span className="font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
+                    <span className='font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center'>
                       Weddings & Receptions
-                      <ArrowRightIcon className="h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out" />
+                      <ArrowRightIcon className='h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out' />
                     </span>
-                    <span className="font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white">
+                    <span className='font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white'>
                       We’d love to put sprinkles on top of your special day.
                     </span>
                   </Link>
@@ -110,11 +110,11 @@ export default async function Nav() {
                     href={`#`}
                     className="bg-repeat bg-white hover:bg-[url('/images/tileDark.png')] p-5 rounded-lg space-y-3 flex flex-col justify-start items-start min-w-[260px] group/lineup-cookies transition-all duration-300 ease-in-out"
                   >
-                    <span className="font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center">
+                    <span className='font-poppins font-bold text-stone-500 text-sm group-hover/lineup-cookies:text-white flex justify-start items-center'>
                       Corporate Events
-                      <ArrowRightIcon className="h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out" />
+                      <ArrowRightIcon className='h-3 w-3 ml-2 group-hover/lineup-cookies:ml-3 transition-all duration-300 ease-in-out' />
                     </span>
-                    <span className="font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white">
+                    <span className='font-poppins text-stone-500 text-xs group-hover/lineup-cookies:text-white'>
                       What’s more fun than charts & graphs? Cookies.
                     </span>
                   </Link>
@@ -122,35 +122,35 @@ export default async function Nav() {
               </div>
             </div>
           </li>
-          <li className="mr-7">
+          <li className='mr-7'>
             <Link
               href={`/about-us`}
-              className="font-poppins font-semibold text-black flex justify-start items-center"
+              className='font-poppins font-semibold text-black flex justify-start items-center'
             >
               about
             </Link>
           </li>
         </ul>
-        <ul className="z-50">
+        <ul className='z-50'>
           <li>
             <Link href={`#`}></Link>
           </li>
           <li>
             <Link
               href={`/cart`}
-              className="text-pink-700 hover:text-pink-950 transition-all duration-100 ease-in-out group"
+              className='text-pink-700 hover:text-pink-950 transition-all duration-100 ease-in-out group'
             >
-              <ShoppingCartIcon className="h-6 w-6 fill-pink-700 group-hover:fill-pink-950 transition-all duration-300 ease-in-out" />
+              <ShoppingCartIcon className='h-6 w-6 fill-pink-700 group-hover:fill-pink-950 transition-all duration-300 ease-in-out' />
             </Link>
           </li>
         </ul>
       </section>
       <Image
         src={`/images/navTile.svg`}
-        className="w-1/4 h-auto absolute top-0 right-0"
+        className='w-1/4 h-auto absolute top-0 right-0'
         width={800}
         height={1000}
-        alt="something"
+        alt='something'
       ></Image>
     </nav>
   );
